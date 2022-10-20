4bdc411130868ac1c11f55a0a8605f8437dc5f26
ramjee12345 <ramjeepentakota425@gmail.com>
2022-10-20 14:32:37 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#hackerschool_Team
+#Beef-XSS installation
+##Packages Download:
+
+wget http://ftp.us.debian.org/debian/pool/main/r/ruby-tzinfo/ruby-tzinfo_1.2.6-1~bpo10+1_all.deb
+wget http://ftp.us.debian.org/debian/pool/main/r/rails/ruby-activesupport_6.0.3.7+dfsg-2_all.deb
+wget http://ftp.us.debian.org/debian/pool/main/r/rails/ruby-activemodel_6.0.3.7+dfsg-2_all.deb
+wget http://ftp.us.debian.org/debian/pool/main/r/rails/ruby-activerecord_6.0.3.7+dfsg-2_all.deb
+
+##Installation:
+
+dpkg -i ruby-tzinfo_1.2.6-1~bpo10+1_all.deb
+dpkg -i ruby-activesupport_6.0.3.7+dfsg-2_all.deb
+dpkg -i ruby-activemodel_6.0.3.7+dfsg-2_all.deb
+dpkg -i ruby-activerecord_6.0.3.7+dfsg-2_all.deb
