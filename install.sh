bebe8c92ab3d23cd5f87db745a0ff424dee1dc79
ramjee <ramjeepentakota425@gmail.com>
2022-12-06 21:08:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ wget http://ftp.us.debian.org/debian/pool/main/r/rails/ruby-activemodel_6.0.3.7+
 wget http://ftp.us.debian.org/debian/pool/main/r/rails/ruby-activerecord_6.0.3.7+dfsg-2_all.deb
 
 ##Installation:
+apt install ruby-tzinfo ruby-concurrent ruby-i18n ruby-zeitwerk
 
 dpkg -i ruby-tzinfo_1.2.6-1~bpo10+1_all.deb
 dpkg -i ruby-activesupport_6.0.3.7+dfsg-2_all.deb
